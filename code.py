3ea33e522cdb0ea1068f461e18d3c04a3a04339d
ParidhiChahal <Paridhichahal574@gmail.com>
2026-03-24 23:31:31 +0530

    Replace matplotlib with Streamlit scatter chart

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+st.scatter_chart(df[['Cost', 'Sales']])
 
 # Load Data
 df = pd.read_csv("Nassau Candy Distributor (1).csv")
